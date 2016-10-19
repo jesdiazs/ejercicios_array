4cb2a2f36e18f6889e177b4ee6026657f583ded0
jesdiazs <jes.diazserrano@gmail.com>
2016-10-19 09:20:06 -0300

    primera actualizacion de los ejercicios de array

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//Ejercicio 1: Funcion con tres enteros y muestre el menor
+
+function comparacionEntero (num1, num2, num3){
+	if (num1<num2 && num1<num3){
+		console.log("el menor de los tres numeros es :"+num1);
+	} else if (num2>num3){
+		console.log("el menor de los tres numeros es :"+num3);
+	}
+	  else if(num3>num2){
+	  	console.log("el menor de los tres numeros es :"+num2);
+	  } 
+}
+
+// Ejercicio 2: Numero Par e Impar
+function parImpar (numero){
+	if (numero%2===0){
+		console.log("El numero introducido es Par"+ " " +numero);
+	} else{
+		console.log("El numero introducido es Impar"+ " " +numero);
+		}
+}
+
+//Ejercicio 3: Ordenando numeros
+function numEnteros(num1, num2, num3){
+	
+}
+
+//Ejercicio 4: Mayuscula o Minusculas
